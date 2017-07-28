b6dc61496d4a0dd24e5ef2b7bf6fcce6ae8a72be
Daniel Salomon <30504863+Salomon-MH@users.noreply.github.com>
2017-07-28 22:13:27 +0200

    Code for IRArdu should work now, including binary output

--- LED_Remote_Vis_DualArduSetup/LED_Remote_Vis_IRArdu/LED_Remote_Vis_IRArdu.ino
+++ LED_Remote_Vis_DualArduSetup/LED_Remote_Vis_IRArdu/LED_Remote_Vis_IRArdu.ino
@@ -4,7 +4,7 @@
 ///// Changelog:
 
 /////// 28.07.2017: - First testing
-///////             
+///////             - Binary encoding seems to work perfectly fine
 ///////             
 ///////             
 
@@ -15,25 +15,22 @@
 //////////Constants
 //PINs
 #define IR_PIN    3  //Pin for the Infrared reciever
-#define OUT_PIN1  5
-#define OUT_PIN2  5
-#define OUT_PIN3  5
+#define OUT_PIN1  5  //The output pins
+#define OUT_PIN2  6
+#define OUT_PIN3  7
+
+//Digital output timeout time
+#define OUTPUT_TIME 70 //70 is a bit more than 2x the delay in loop() for the LED Arduino. Should be timeout enough!
 
 //IR HEX values
-uint32_t IR_CHANGEVISUAL = 0x20DF04FB;//0x20DF04FB; //Example HEX value, change to actual values!
-uint32_t IR_CHANGECOLOR = 0x20DF847B;//0x20DF847B;
-uint32_t IR_CHANGEBRIGHTNESS = 0x20DFF906;//0x20DFF906;
-
-//////////<Globals>
-//  These values either need to be remembered from the last pass of loop() or 
-//  need to be accessed by several functions in one pass, so they need to be global.
+uint32_t IR_signal_1 = 0x20DF04FB; //Used to be IR_CHANGEVISUAL //LG TV remote HEX values, change to your actual values!
+uint32_t IR_signal_2 = 0x20DF847B; //Used to be IR_CHANGECOLOR
+uint32_t IR_signal_3 = 0x20DFF906; //Used to be IR_CHANGEBRIGHTNESS
 
 //Infrared
 IRrecv irrecv(IR_PIN);
 IRdecode irDecoder;
 uint32_t irValue;
-
-//////////</Globals>
 
 
 //////////<Standard Functions>
@@ -42,50 +39,70 @@ void setup() {    //Like it's named, this gets ran before any other function.
 
   Serial.begin(9600); //Sets data rate for serial data transmission.
 
-  //Defines the buttons pins to be input.
-  //pinMode(BUTTON_1, INPUT); pinMode(BUTTON_2, INPUT); pinMode(BUTTON_3, INPUT);
+  //Defines the pins to be output.
+  pinMode(OUT_PIN1, OUTPUT);
+  pinMode(OUT_PIN2, OUTPUT);
+  pinMode(OUT_PIN3, OUTPUT);
 
-  //Write a "HIGH" value to the button pins.
-  //digitalWrite(BUTTON_1, HIGH); digitalWrite(BUTTON_2, HIGH); digitalWrite(BUTTON_3, HIGH);
+  //Write a "LOW" value to the pins to initialize.
+  digitalWrite(OUT_PIN1, LOW);
+  digitalWrite(OUT_PIN2, LOW);
+  digitalWrite(OUT_PIN3, LOW);
   
   irrecv.enableIRIn(); // Start the infrared receiver
 }
 
 
-void loop() {  //This is where the magic happens. This loop produces each frame of the visual.
+void loop() {
 
   irValue = 0;
   if (irrecv.getResults())
   {
      irDecoder.decode();
 
-     if (irDecoder.value == 0) { //As the animation interrupts IR recieving, you have to press the same button two times in a row
-       showIRisListening();
-       for (int retryCnt = 0; retryCnt < 20; retryCnt++) { //I honestly have absolutely **NO IDEA** why this works, but it works like a charm!
-        irrecv.disableIRIn();
-        irrecv.enableIRIn();
-        delay(200);
-        irrecv.getResults();
-        irDecoder.decode();
-        if (irDecoder.value == IR_CHANGEVISUAL || irDecoder.value == IR_CHANGECOLOR || irDecoder.value == IR_CHANGEBRIGHTNESS) {
-          retryCnt = 20;
-          irValue = irDecoder.value;
+        if (irDecoder.value != 0xFFFFFFFF) {
+			irValue = irDecoder.value; //Saves the HEX result recieved.
         }
-       }
-     }
-     //irValue = irDecoder.value;
-
-     /**if (irDecoder.value != 0xFFFFFFFF)
-     {
-      irValue = irDecoder.value; //Detect the HEX results recieved.
-      //Serial.println(irValue, HEX);
-     }**/
      
-     irrecv.enableIRIn(); // Receive the next infrared value
-  } //irResults.value should be IR_CHANGEBRIGHTNESS, IR_CHANGECOLOR or IR_CHANGEVISUAL now, otherwise nothing should happen.
+     irrecv.enableIRIn(); // Begin receiving the next infrared value
+  }
+  //irResults.value should be a valid IR signal now, otherwise nothing will happen.
+
+  if (irValue != 0) {
+    encodeSignal();
+    resetOutput();
+  }
   
-  //volume = analogRead(AUDIO_PIN);       //Record the volume level from the sound detector
-     //Paces visuals so they aren't too fast to be enjoyable
+}
+
+void encodeSignal() {
+	int recievedValue = -1;
+	if(irValue == IR_signal_1) recievedValue = 1;
+  else if(irValue == IR_signal_2) recievedValue = 2;
+  else if(irValue == IR_signal_3) recievedValue = 3;
+  
+  Serial.println(recievedValue);
+	if (recievedValue != -1) {
+		encodeSignal_helper(recievedValue);
+	}
+}
+
+void encodeSignal_helper(int value) {
+	int output[3];
+	for (int i = 0; i < 3; i++) {
+		output[i] = value%2;
+		value = value/2;
+	} //if value=1 output is 100, for 2 it is 010, for 3 it is 110 etc.
+	if (output[0]) digitalWrite(OUT_PIN1, HIGH);
+  if (output[1]) digitalWrite(OUT_PIN2, HIGH);
+  if (output[2]) digitalWrite(OUT_PIN3, HIGH);
+}
+
+void resetOutput() {
+	delay(OUTPUT_TIME);
+	  digitalWrite(OUT_PIN1, LOW);
+    digitalWrite(OUT_PIN2, LOW);
+    digitalWrite(OUT_PIN3, LOW);
 }
 
 //////////</Standard Functions>
